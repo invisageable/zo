40c1d32b061bb62b7c8d19e82fb9eb8cc3fdc5be
invisageable <iam@compilords.house>
2024-03-02 11:59:54 +0100

    zhoo — improves `ty`.

---
+++ crates/compiler/zhoo-ty/src/pretty.rs
@@ -0,0 +1,24 @@
+use super::ty::{Ty, TyKind};
+
+impl std::fmt::Display for Ty {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "{}", self.kind)
+  }
+}
+
+impl std::fmt::Display for TyKind {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    match self {
+      Self::Unit => write!(f, "()"),
+      Self::Int => write!(f, "int"),
+      Self::Float => write!(f, "float"),
+      Self::Ident(ident) => write!(f, "{ident}"),
+      Self::Bool => write!(f, "bool"),
+      Self::Char => write!(f, "char"),
+      Self::Str => write!(f, "str"),
+      Self::Fun => write!(f, "fun"),
+      Self::Infer => write!(f, "_"),
+      Self::Custom(ident) => write!(f, "{ident}"),
+    }
+  }
+}
--- crates/compiler/zhoo-ty/src/ty.rs
+++ crates/compiler/zhoo-ty/src/ty.rs
@@ -1,39 +1,83 @@
-use serde::{Deserialize, Serialize};
+//! ...
 
-#[derive(Debug, Serialize, Deserialize)]
+use zo_core::interner::symbol::Symbol;
+use zo_core::span::Span;
+
+#[derive(Clone, Debug)]
 pub struct Ty {
   pub kind: TyKind,
+  pub span: Span,
 }
 
 impl Ty {
-  pub const UNIT: Self = Self::of(TyKind::Int);
+  pub const UNIT: Self = Self::of(TyKind::Int, Span::ZERO);
 
-  pub const fn of(kind: TyKind) -> Self {
-    Self { kind }
+  pub const fn of(kind: TyKind, span: Span) -> Self {
+    Self { kind, span }
   }
 
-  pub const fn fun() -> Self {
-    Self::of(TyKind::Fun)
+  pub const fn int(span: Span) -> Self {
+    Self::of(TyKind::Int, span)
   }
 
-  pub const fn int() -> Self {
-    Self::of(TyKind::Int)
+  pub const fn float(span: Span) -> Self {
+    Self::of(TyKind::Float, span)
   }
 
-  pub const fn float() -> Self {
-    Self::of(TyKind::Float)
+  pub const fn ident(ident: String, span: Span) -> Self {
+    Self::of(TyKind::Ident(ident), span)
   }
 
-  pub const fn ident(ident: String) -> Self {
-    Self::of(TyKind::Ident(ident))
+  pub const fn bool(span: Span) -> Self {
+    Self::of(TyKind::Bool, span)
+  }
+
+  pub const fn char(span: Span) -> Self {
+    Self::of(TyKind::Char, span)
+  }
+
+  pub const fn str(span: Span) -> Self {
+    Self::of(TyKind::Str, span)
+  }
+
+  pub const fn fun(span: Span) -> Self {
+    Self::of(TyKind::Fun, span)
+  }
+
+  pub const fn custom(ident: Symbol, span: Span) -> Self {
+    Self::of(TyKind::Custom(ident), span)
   }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+impl From<(&str, Span)> for Ty {
+  fn from((ident, span): (&str, Span)) -> Self {
+    match ident {
+      "int" => Ty::int(span),
+      "float" => Ty::float(span),
+      "bool" => Ty::bool(span),
+      "char" => Ty::char(span),
+      "str" => Ty::str(span),
+      _ => Ty::custom(find_me(ident), span),
+    }
+  }
+}
+
+#[derive(Clone, Debug)]
 pub enum TyKind {
   Unit,
   Int,
   Float,
   Ident(String),
+  Bool,
+  Char,
+  Str,
   Fun,
+  Infer,
+  Custom(Symbol),
+}
+
+// todo(ivs) — find the related symbol from the interner symbol table.
+// it should be implemented on the `parser` side.
+fn find_me(_ident: &str) -> Symbol {
+  todo!()
 }
