1376c28e0ce450ad85320b8d6f0bca8f8f92388d
invisageable <iam@compilords.house>
2024-03-28 00:01:32 +0100

    zhyr — wip.

--- crates/booster/zhyr-codegen-py/src/translator.rs
+++ crates/booster/zhyr-codegen-py/src/translator.rs
@@ -1,3 +1,5 @@
+//! ...
+
 use zhyr_ast::ast::{Ast, File, FileKind, ItemKind, Module, ModuleKind};
 
 use zo_core::writer::Writer;
@@ -11,7 +13,7 @@ impl Translator {
   #[inline]
   pub fn new() -> Self {
     Self {
-      writer: Writer::new(2usize),
+      writer: Writer::new(0usize),
     }
   }
 
@@ -65,7 +67,6 @@ impl Translator {
   ) -> Result<()> {
     for stmt in &module.body {
       self.translate_stmt(stmt)?;
-      println!("\n\nSTMT: {:?}\n", stmt);
     }
 
     Ok(())
@@ -97,10 +98,81 @@ impl Translator {
       rustpython_ast::Stmt::FunctionDef(function_def) => {
         self.translate_stmt_function_def(function_def)
       }
+      rustpython_ast::Stmt::AsyncFunctionDef(_asymc_function_def) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::ClassDef(_class_def) => {
+        todo!()
+      }
       rustpython_ast::Stmt::Return(ret) => self.translate_stmt_return(ret),
+      rustpython_ast::Stmt::Delete(_delete) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Assign(_assign) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::TypeAlias(_type_alias) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::AugAssign(_aug_assign) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::AnnAssign(_ann_assign) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::For(_for) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::AsyncFor(_async_for) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::While(_while) => {
+        todo!()
+      }
       rustpython_ast::Stmt::If(if_else) => self.translate_stmt_if_else(if_else),
+      rustpython_ast::Stmt::With(_with) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::AsyncWith(_async_with) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Match(_matching) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Raise(_raise) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Try(_trying) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::TryStar(_try_start) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Assert(_assert) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Import(_import) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::ImportFrom(_import_from) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Global(_global) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Nonlocal(_non_local) => {
+        todo!()
+      }
       rustpython_ast::Stmt::Expr(expr) => self.translate_stmt_expr(expr),
-      kind => todo!("{kind:?}"),
+      rustpython_ast::Stmt::Pass(_pass) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Break(_breaking) => {
+        todo!()
+      }
+      rustpython_ast::Stmt::Continue(_continue) => {
+        todo!()
+      }
     }
   }
 
@@ -112,7 +184,6 @@ impl Translator {
     self.writer.space()?;
     self.translate_identifier(&function_def.name)?;
     self.translate_arguments(&function_def.args)?;
-    self.writer.space()?;
     self.translate_body(&function_def.body)
   }
 
@@ -126,7 +197,9 @@ impl Translator {
     match &ret.value {
       Some(expr) => self.translate_expr(expr),
       None => Ok(()),
-    }
+    }?;
+
+    self.writer.semicolon()
   }
 
   fn translate_identifier(
@@ -142,8 +215,13 @@ impl Translator {
   ) -> Result<()> {
     self.writer.write_bytes(b"(")?;
 
-    for arg_with_default in &args.args {
+    for (idx, arg_with_default) in args.args.iter().enumerate() {
       self.translate_argument_with_default(arg_with_default)?;
+
+      if idx < args.args.len() - 1 {
+        self.writer.comma()?;
+        self.writer.space()?;
+      }
     }
 
     self.writer.write_bytes(b")")
@@ -164,13 +242,20 @@ impl Translator {
     &mut self,
     stmts: &Vec<rustpython_ast::Stmt>,
   ) -> Result<()> {
+    self.writer.space()?;
     self.writer.write_bytes(b"{")?;
+    self.writer.new_line()?;
 
-    for stmt in stmts {
+    for (idx, stmt) in stmts.iter().enumerate() {
       self.translate_stmt(stmt)?;
+
+      if idx < stmts.len() - 1 {
+        self.writer.new_line()?;
+      }
     }
 
-    self.writer.write_bytes(b"}")
+    self.writer.write_bytes(b"}")?;
+    self.writer.new_line()
   }
 
   fn translate_stmt_if_else(
@@ -195,17 +280,84 @@ impl Translator {
 
   fn translate_expr(&mut self, expr: &rustpython_ast::Expr) -> Result<()> {
     match expr {
+      rustpython_ast::Expr::BoolOp(bool_op) => {
+        self.translate_expr_bool_op(bool_op)
+      }
+      rustpython_ast::Expr::NamedExpr(named) => {
+        self.translate_expr_named(named)
+      }
       rustpython_ast::Expr::BinOp(binop) => self.translate_expr_binop(binop),
+      rustpython_ast::Expr::UnaryOp(unop) => self.translate_expr_unop(unop),
+      rustpython_ast::Expr::Lambda(lambda) => {
+        self.translate_expr_lambda(lambda)
+      }
+      rustpython_ast::Expr::IfExp(if_else) => {
+        self.translate_expr_if_else(if_else)
+      }
+      rustpython_ast::Expr::Dict(dict) => self.translate_expr_dict(dict),
+      rustpython_ast::Expr::Set(set) => self.translate_expr_set(set),
+      rustpython_ast::Expr::ListComp(list_comp) => {
+        self.translate_expr_list_comp(list_comp)
+      }
+      rustpython_ast::Expr::SetComp(set_comp) => {
+        self.translate_expr_set_comp(set_comp)
+      }
+      rustpython_ast::Expr::DictComp(dict_comp) => {
+        self.translate_expr_dict_comp(dict_comp)
+      }
+      rustpython_ast::Expr::GeneratorExp(generator) => {
+        self.translate_expr_generator(generator)
+      }
+      rustpython_ast::Expr::Await(awaiting) => {
+        self.translate_expr_await(awaiting)
+      }
+      rustpython_ast::Expr::Yield(yielding) => {
+        self.translate_expr_yield(yielding)
+      }
+      rustpython_ast::Expr::YieldFrom(yield_from) => {
+        self.translate_expr_yield_from(yield_from)
+      }
       rustpython_ast::Expr::Compare(compare) => {
         self.translate_expr_compare(compare)
       }
       rustpython_ast::Expr::Call(call) => self.translate_expr_call(call),
+      rustpython_ast::Expr::FormattedValue(formatted_value) => {
+        self.translate_expr_formatted_value(formatted_value)
+      }
+      rustpython_ast::Expr::JoinedStr(joined_str) => {
+        self.translate_expr_joined_str(joined_str)
+      }
       rustpython_ast::Expr::Constant(constant) => {
         self.translate_expr_constant(constant)
       }
+      rustpython_ast::Expr::Attribute(attribute) => {
+        self.translate_expr_attribute(attribute)
+      }
+      rustpython_ast::Expr::Subscript(subscript) => {
+        self.translate_expr_subscript(subscript)
+      }
+      rustpython_ast::Expr::Starred(starred) => {
+        self.translate_expr_starred(starred)
+      }
       rustpython_ast::Expr::Name(name) => self.translate_expr_name(name),
-      kind => todo!("\n\n{kind:#?}\n"),
+      rustpython_ast::Expr::List(list) => self.translate_expr_list(list),
+      rustpython_ast::Expr::Tuple(tuple) => self.translate_expr_tuple(tuple),
+      rustpython_ast::Expr::Slice(slice) => self.translate_expr_slice(slice),
     }
+  }
+
+  fn translate_expr_bool_op(
+    &mut self,
+    _bool_op: &rustpython_ast::ExprBoolOp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_named(
+    &mut self,
+    _named: &rustpython_ast::ExprNamedExpr,
+  ) -> Result<()> {
+    todo!()
   }
 
   fn translate_expr_binop(
@@ -234,6 +386,90 @@ impl Translator {
       rustpython_ast::Operator::FloorDiv => self.writer.write_bytes(b"/"),
       _ => todo!(),
     }
+  }
+
+  fn translate_expr_unop(
+    &mut self,
+    _unop: &rustpython_ast::ExprUnaryOp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_lambda(
+    &mut self,
+    _lambda: &rustpython_ast::ExprLambda,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_if_else(
+    &mut self,
+    _if_else: &rustpython_ast::ExprIfExp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_dict(
+    &mut self,
+    _dict: &rustpython_ast::ExprDict,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_set(
+    &mut self,
+    _set: &rustpython_ast::ExprSet,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_list_comp(
+    &mut self,
+    _list_cpom: &rustpython_ast::ExprListComp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_set_comp(
+    &mut self,
+    _set_comp: &rustpython_ast::ExprSetComp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_dict_comp(
+    &mut self,
+    _dict_comp: &rustpython_ast::ExprDictComp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_generator(
+    &mut self,
+    _generator: &rustpython_ast::ExprGeneratorExp,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_await(
+    &mut self,
+    _awaiting: &rustpython_ast::ExprAwait,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_yield(
+    &mut self,
+    _yielding: &rustpython_ast::ExprYield,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_yield_from(
+    &mut self,
+    _yield_from: &rustpython_ast::ExprYieldFrom,
+  ) -> Result<()> {
+    todo!()
   }
 
   fn translate_expr_compare(
@@ -278,6 +514,27 @@ impl Translator {
     }
 
     self.writer.write_bytes(b")")
+  }
+
+  fn translate_expr_formatted_value(
+    &mut self,
+    _formatted_value: &rustpython_ast::ExprFormattedValue,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_joined_str(
+    &mut self,
+    _joined_str: &rustpython_ast::ExprJoinedStr,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_attribute(
+    &mut self,
+    _attribute: &rustpython_ast::ExprAttribute,
+  ) -> Result<()> {
+    todo!()
   }
 
   fn translate_expr_constant(
@@ -357,11 +614,46 @@ impl Translator {
     todo!()
   }
 
+  fn translate_expr_subscript(
+    &mut self,
+    _subscript: &rustpython_ast::ExprSubscript,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_starred(
+    &mut self,
+    _starred: &rustpython_ast::ExprStarred,
+  ) -> Result<()> {
+    todo!()
+  }
+
   fn translate_expr_name(
     &mut self,
     name: &rustpython_ast::ExprName,
   ) -> Result<()> {
     self.translate_identifier(&name.id)
+  }
+
+  fn translate_expr_list(
+    &mut self,
+    _list: &rustpython_ast::ExprList,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_tuple(
+    &mut self,
+    _tuple: &rustpython_ast::ExprTuple,
+  ) -> Result<()> {
+    todo!()
+  }
+
+  fn translate_expr_slice(
+    &mut self,
+    _slice: &rustpython_ast::ExprSlice,
+  ) -> Result<()> {
+    todo!()
   }
 
   fn translate_exprs(
