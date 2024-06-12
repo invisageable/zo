8f528743fb8bc4e62565114781ceb34895bcaa2d
invisageable <iam@compilords.house>
2024-06-12 12:45:51 +0200

    zo — implements `repl`.

--- crates/compiler/zo-driver/src/cmd/repl.rs
+++ crates/compiler/zo-driver/src/cmd/repl.rs
@@ -2,12 +2,20 @@
 
 use crate::cmd::Handle;
 
-use clap::Parser;
-
-use zo_core::{Result, EXIT_FAILURE, EXIT_SUCCESS};
+use zo_compiler::compiler::Compiler;
+use zo_compiler::phase::analyzing::Analyzing;
+use zo_compiler::phase::interpreting::Interpreting;
+use zo_compiler::phase::parsing::Parsing;
+use zo_compiler::phase::reading::Reading;
+use zo_compiler::phase::tokenizing::Tokenizing;
+use zo_compiler::phase::Phase;
 use zo_session::session::Session;
 use zo_session::settings::Settings;
 
+use zo_core::mpsc::channel;
+use zo_core::{Result, EXIT_FAILURE, EXIT_SUCCESS};
+
+use clap::Parser;
 use smol_str::SmolStr;
 
 #[derive(Parser)]
@@ -46,7 +54,44 @@ impl Repl {
 
     session.open();
 
-    Ok(())
+    loop {
+      // todo (ivs) — use a better implementation instead of that ugly
+      // implementation of channel. it does the job done for now but in the
+      // future maybe, a better approach should be written.
+      let (rx_reading, tx_reading) = channel::bounded(channel::CAPACITY);
+      let (rx_tokenizing, tx_tokenizing) = channel::bounded(channel::CAPACITY);
+      let (rx_parsing, tx_parsing) = channel::bounded(channel::CAPACITY);
+      let (rx_analyzing, tx_analyzing) = channel::bounded(channel::CAPACITY);
+
+      let (rx_interpreting, tx_interpreting) =
+        channel::bounded(channel::CAPACITY);
+
+      let compiler = Compiler::new()
+        .add_phase(Phase::Reading(Reading { rx: rx_reading }))
+        .add_phase(Phase::Tokenizing(Tokenizing {
+          rx: rx_tokenizing,
+          tx: tx_reading,
+        }))
+        .add_phase(Phase::Parsing(Parsing {
+          rx: rx_parsing,
+          tx: tx_tokenizing,
+        }))
+        .add_phase(Phase::Analyzing(Analyzing {
+          rx: rx_analyzing,
+          tx: tx_parsing,
+        }))
+        .add_phase(Phase::Interpreting(Interpreting {
+          rx: rx_interpreting,
+          tx: tx_analyzing,
+        }));
+
+      compiler.compile(&mut session)?;
+
+      compiler.finish(tx_interpreting).map(|value| {
+        println!("{value}");
+        session.close();
+      })?;
+    }
   }
 }
 
