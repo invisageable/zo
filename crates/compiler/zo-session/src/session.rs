6dfb830eb80514a669689c6b40d743a7a8a0d911
invisageable <iam@compilords.house>
2024-08-01 02:59:11 +0200

    zo — updates `session`.

--- crates/compiler/zo-session/src/session.rs
+++ crates/compiler/zo-session/src/session.rs
@@ -1,14 +1,26 @@
 use zo_interner::interner::Interner;
+use zo_reporter::reporter::Reporter;
 
+use swisskit::profiler::Profiler;
+
+/// The representation of a compiler's session.
 pub struct Session {
+  /// The string interner.
   pub interner: Interner,
+  /// The diagnostic reporter.
+  pub reporter: Reporter,
+  /// The time profiler.
+  pub profiler: Profiler,
 }
 
 impl Session {
+  /// Creates a new session.
   #[inline]
   pub fn new() -> Self {
     Self {
       interner: Interner::new(),
+      reporter: Reporter::new(),
+      profiler: Profiler::new(),
     }
   }
 }
