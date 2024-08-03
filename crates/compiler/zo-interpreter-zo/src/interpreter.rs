c25ca1ebeffe7492c9c24fa997cf7ca8b7b21176
invisageable <iam@compilords.house>
2024-08-03 15:00:05 +0200

    zo — updates `zo-interpreter-zo` to support `binop`.

--- crates/compiler/zo-interpreter-zo/src/interpreter.rs
+++ crates/compiler/zo-interpreter-zo/src/interpreter.rs
@@ -1,10 +1,13 @@
-use zo_ast::ast::{Ast, Expr, ExprKind, Lit, LitKind, Stmt, StmtKind};
+use zo_ast::ast::{
+  Ast, BinOp, BinOpKind, Expr, ExprKind, Lit, LitKind, Stmt, StmtKind, UnOp,
+};
+
 use zo_interner::interner::symbol::Symbol;
 use zo_interner::interner::Interner;
 use zo_reporter::reporter::Reporter;
 use zo_reporter::Result;
 use zo_session::session::Session;
-use zo_value::value::Value;
+use zo_value::value::{Value, ValueKind};
 
 use swisskit::span::Span;
 
@@ -53,6 +56,12 @@ impl<'ast> Interpreter<'ast> {
   fn interpret_expr(&mut self, expr: &Expr) -> Result<Value> {
     match &expr.kind {
       ExprKind::Lit(lit) => self.interpret_expr_lit(lit),
+      ExprKind::UnOp(unop, rhs) => {
+        self.interpret_expr_unop(unop, rhs, expr.span)
+      }
+      ExprKind::BinOp(binop, lhs, rhs) => {
+        self.interpret_expr_binop(binop, lhs, rhs, expr.span)
+      }
       _ => todo!(),
     }
   }
@@ -74,6 +83,46 @@ impl<'ast> Interpreter<'ast> {
     let int = self.interner.lookup_int(**sym as usize);
 
     Ok(Value::int(int, span))
+  }
+
+  fn interpret_expr_unop(
+    &mut self,
+    unop: &UnOp,
+    rhs: &Expr,
+    span: Span,
+  ) -> Result<Value> {
+    todo!()
+  }
+
+  fn interpret_expr_binop(
+    &mut self,
+    binop: &BinOp,
+    lhs: &Expr,
+    rhs: &Expr,
+    span: Span,
+  ) -> Result<Value> {
+    let lhs = self.interpret_expr(lhs)?;
+    let rhs = self.interpret_expr(rhs)?;
+
+    match (&lhs.kind, &rhs.kind) {
+      (ValueKind::Int(lhs), ValueKind::Int(rhs)) => {
+        self.interpret_expr_binop_int(binop, lhs, rhs, span)
+      }
+      _ => todo!(),
+    }
+  }
+
+  fn interpret_expr_binop_int(
+    &mut self,
+    binop: &BinOp,
+    lhs: &i64,
+    rhs: &i64,
+    span: Span,
+  ) -> Result<Value> {
+    match binop.kind {
+      BinOpKind::Add => Ok(Value::int(lhs + rhs, span)),
+      _ => todo!(),
+    }
   }
 }
 
