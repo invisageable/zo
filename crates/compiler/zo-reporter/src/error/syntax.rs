f92f8bdc6d57c9cc7abc9f505b7833aa6abfbb2f
invisageable <iam@compilords.house>
2024-08-03 02:57:55 +0200

    zo — updates `syntax` error.

--- crates/compiler/zo-reporter/src/error/syntax.rs
+++ crates/compiler/zo-reporter/src/error/syntax.rs
@@ -1,15 +1,38 @@
-use super::Diagnostic;
+use super::{Diagnostic, Error};
 
 use crate::report::Report;
 
+use smol_str::SmolStr;
 use swisskit::span::Span;
 
 /// The representation of syntax analysis errors.
 #[derive(Debug)]
-pub enum Syntax {}
+pub enum Syntax {
+  ExpectedInt(Span, SmolStr),
+  InvalidInfix(Span, SmolStr),
+  InvalidPrefix(Span, SmolStr),
+}
 
 impl<'a> Diagnostic<'a> for Syntax {
   fn report(&self) -> Report<'a> {
     todo!()
   }
+}
+
+/// The expected integer literal error.
+#[inline]
+pub const fn expected_int(span: Span, token: SmolStr) -> Error {
+  Error::Syntax(Syntax::ExpectedInt(span, token))
+}
+
+/// The invalid infix error.
+#[inline]
+pub const fn invalid_infix(span: Span, token: SmolStr) -> Error {
+  Error::Syntax(Syntax::InvalidInfix(span, token))
+}
+
+/// The invalid prefix error.
+#[inline]
+pub const fn invalid_prefix(span: Span, token: SmolStr) -> Error {
+  Error::Syntax(Syntax::InvalidPrefix(span, token))
 }
--- crates/compiler/zo-tokenizer/src/token.rs
+++ crates/compiler/zo-tokenizer/src/token.rs
@@ -18,9 +18,15 @@ pub mod int;
 pub mod kw;
 pub mod punctuation;
 
+use group::Group;
+use kw::Kw;
+use punctuation::Punctuation;
+
 use zo_interner::interner::symbol::Symbol;
 
 use swisskit::span::Span;
+
+use smol_str::{SmolStr, ToSmolStr};
 
 /// The representation of a token.
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -45,6 +51,12 @@ impl Token {
   #[inline]
   pub fn is(&self, kind: TokenKind) -> bool {
     self.kind.is(kind)
+  }
+}
+
+impl ToSmolStr for Token {
+  fn to_smolstr(&self) -> smol_str::SmolStr {
+    self.kind.to_smolstr()
   }
 }
 
@@ -82,5 +94,153 @@ impl TokenKind {
   #[inline]
   pub fn is(&self, kind: TokenKind) -> bool {
     *self == kind
+  }
+
+  #[inline]
+  pub fn is_assignement(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::Equal)
+        | Self::Punctuation(Punctuation::PlusEqual)
+        | Self::Punctuation(Punctuation::MinusEqual)
+        | Self::Punctuation(Punctuation::AsteriskEqual)
+        | Self::Punctuation(Punctuation::SlashEqual)
+        | Self::Punctuation(Punctuation::PercentEqual)
+        | Self::Punctuation(Punctuation::CircumflexEqual)
+        | Self::Punctuation(Punctuation::AmspersandEqual)
+        | Self::Punctuation(Punctuation::PipeEqual)
+        | Self::Punctuation(Punctuation::LessThanLessThanEqual)
+        | Self::Punctuation(Punctuation::GreaterThanGreaterThanEqual)
+    )
+  }
+
+  #[inline]
+  pub fn is_conditional(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::AmpersandAmpersand)
+        | Self::Punctuation(Punctuation::PipePipe)
+    )
+  }
+
+  #[inline]
+  pub fn is_comparison(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::EqualEqual)
+        | Self::Punctuation(Punctuation::ExclamationEqual)
+        | Self::Punctuation(Punctuation::LessThan)
+        | Self::Punctuation(Punctuation::GreaterThan)
+        | Self::Punctuation(Punctuation::LessThanEqual)
+        | Self::Punctuation(Punctuation::GreaterThanEqual)
+    )
+  }
+
+  #[inline]
+  pub fn is_sum(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::Plus)
+        | Self::Punctuation(Punctuation::Minus)
+    )
+  }
+
+  #[inline]
+  pub fn is_exponent(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::Asterisk)
+        | Self::Punctuation(Punctuation::Slash)
+        | Self::Punctuation(Punctuation::Percent)
+    )
+  }
+
+  #[inline]
+  pub fn is_calling(&self) -> bool {
+    matches!(self, Self::Group(Group::ParenOpen))
+  }
+
+  #[inline]
+  pub fn is_index(&self) -> bool {
+    matches!(self, Self::Group(Group::BracketOpen))
+  }
+
+  #[inline]
+  pub fn is_chaining(&self) -> bool {
+    matches!(self, Self::Punctuation(Punctuation::Period))
+  }
+
+  #[inline]
+  pub fn is_range(&self) -> bool {
+    matches!(self, Self::Punctuation(Punctuation::PeriodPeriod))
+  }
+
+  #[inline]
+  pub fn is_unop(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::Exclamation)
+        | Self::Punctuation(Punctuation::Minus)
+    )
+  }
+
+  #[inline]
+  pub fn is_binop(&self) -> bool {
+    matches!(
+      self,
+      Self::Punctuation(Punctuation::Plus)
+        | Self::Punctuation(Punctuation::Minus)
+        | Self::Punctuation(Punctuation::Asterisk)
+        | Self::Punctuation(Punctuation::Slash)
+        | Self::Punctuation(Punctuation::Percent)
+        | Self::Punctuation(Punctuation::Circumflex)
+        | Self::Punctuation(Punctuation::EqualEqual)
+        | Self::Punctuation(Punctuation::ExclamationEqual)
+        | Self::Punctuation(Punctuation::LessThan)
+        | Self::Punctuation(Punctuation::GreaterThan)
+        | Self::Punctuation(Punctuation::LessThanEqual)
+        | Self::Punctuation(Punctuation::GreaterThanEqual)
+    )
+  }
+
+  #[inline]
+  pub fn is_item(&self) -> bool {
+    matches!(
+      self,
+      Self::Kw(Kw::Load)
+        | Self::Kw(Kw::Val)
+        | Self::Kw(Kw::Type)
+        | Self::Kw(Kw::Ext)
+        | Self::Kw(Kw::Struct)
+        | Self::Kw(Kw::Fun)
+    )
+  }
+
+  #[inline]
+  pub fn is_var_local(&self) -> bool {
+    matches!(self, Self::Kw(Kw::Imu) | Self::Kw(Kw::Mut))
+  }
+}
+
+impl ToSmolStr for TokenKind {
+  fn to_smolstr(&self) -> smol_str::SmolStr {
+    match self {
+      Self::Eof => SmolStr::new_inline("EOF"),
+      Self::Unknown => SmolStr::new_inline("UNKNOWN"),
+      Self::Space => SmolStr::new_inline("SPACE"),
+      Self::Eol => SmolStr::new_inline("EOL"),
+      Self::Int(sym, base) => {
+        SmolStr::new_inline(format!("{sym}--base--{base:?}").as_str())
+      }
+      Self::Float(sym) => SmolStr::new_inline(format!("{sym}").as_str()),
+      Self::Punctuation(punctuation) => {
+        SmolStr::new_inline(format!("{punctuation}").as_str())
+      }
+      Self::Group(group) => SmolStr::new_inline(format!("{group}").as_str()),
+      Self::Ident(sym) => SmolStr::new_inline(format!("{sym}").as_str()),
+      Self::Kw(sym) => SmolStr::new_inline(format!("{sym}").as_str()),
+      Self::Char(sym) => SmolStr::new_inline(format!("{sym}").as_str()),
+      Self::Str(sym) => SmolStr::new_inline(format!("{sym}").as_str()),
+    }
   }
 }
