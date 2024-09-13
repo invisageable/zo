715f7354314d68b91f2f156a28374acc946c93dc
invisageable <iam@compilords.house>
2024-09-13 16:59:51 +0200

    zo — updates `zo-wasm`.

--- crates/compiler/zo-tokenizer/src/token/kw.rs
+++ crates/compiler/zo-tokenizer/src/token/kw.rs
@@ -1,10 +1,10 @@
 use super::TokenKind;
 
-use german_str::GermanStr;
 use hashbrown::HashMap;
+use smol_str::SmolStr;
 
 /// The keyword dictionnary.
-type Keywords = HashMap<GermanStr, TokenKind>;
+type Keywords = HashMap<SmolStr, TokenKind>;
 
 /// The representation of a keyword.
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -89,39 +89,39 @@ impl std::fmt::Display for Kw {
 lazy_static::lazy_static! {
   // A static map of available keywords.
   pub static ref KEYWORDS: Keywords = HashMap::from([
-    (GermanStr::new_inline("abstract"), TokenKind::Kw(Kw::Abstract)),
-    (GermanStr::new_inline("and"), TokenKind::Kw(Kw::And)),
-    (GermanStr::new_inline("apply"), TokenKind::Kw(Kw::Apply)),
-    (GermanStr::new_inline("as"), TokenKind::Kw(Kw::As)),
-    (GermanStr::new_inline("async"), TokenKind::Kw(Kw::Async)),
-    (GermanStr::new_inline("await"), TokenKind::Kw(Kw::Await)),
-    (GermanStr::new_inline("else"), TokenKind::Kw(Kw::Else)),
-    (GermanStr::new_inline("enum"), TokenKind::Kw(Kw::Enum)),
-    (GermanStr::new_inline("ext"), TokenKind::Kw(Kw::Ext)),
-    (GermanStr::new_inline("false"), TokenKind::Kw(Kw::False)),
-    (GermanStr::new_inline("Fn"), TokenKind::Kw(Kw::FnUpper)),
-    (GermanStr::new_inline("fn"), TokenKind::Kw(Kw::FnLower)),
-    (GermanStr::new_inline("fun"), TokenKind::Kw(Kw::Fun)),
-    (GermanStr::new_inline("for"), TokenKind::Kw(Kw::For)),
-    (GermanStr::new_inline("if"), TokenKind::Kw(Kw::If)),
-    (GermanStr::new_inline("imu"), TokenKind::Kw(Kw::Imu)),
-    (GermanStr::new_inline("load"), TokenKind::Kw(Kw::Load)),
-    (GermanStr::new_inline("loop"), TokenKind::Kw(Kw::Loop)),
-    (GermanStr::new_inline("match"), TokenKind::Kw(Kw::Match)),
-    (GermanStr::new_inline("me"), TokenKind::Kw(Kw::Me)),
-    (GermanStr::new_inline("mut"), TokenKind::Kw(Kw::Mut)),
-    (GermanStr::new_inline("pack"), TokenKind::Kw(Kw::Pack)),
-    (GermanStr::new_inline("pub"), TokenKind::Kw(Kw::Pub)),
-    (GermanStr::new_inline("return"), TokenKind::Kw(Kw::Return)),
-    (GermanStr::new_inline("skip"), TokenKind::Kw(Kw::Skip)),
-    (GermanStr::new_inline("stop"), TokenKind::Kw(Kw::Stop)),
-    (GermanStr::new_inline("struct"), TokenKind::Kw(Kw::Struct)),
-    (GermanStr::new_inline("true"), TokenKind::Kw(Kw::True)),
-    (GermanStr::new_inline("type"), TokenKind::Kw(Kw::Type)),
-    (GermanStr::new_inline("_"), TokenKind::Kw(Kw::Underscore)),
-    (GermanStr::new_inline("val"), TokenKind::Kw(Kw::Val)),
-    (GermanStr::new_inline("wasm"), TokenKind::Kw(Kw::Wasm)),
-    (GermanStr::new_inline("when"), TokenKind::Kw(Kw::When)),
-    (GermanStr::new_inline("while"), TokenKind::Kw(Kw::While)),
+    (SmolStr::new_inline("abstract"), TokenKind::Kw(Kw::Abstract)),
+    (SmolStr::new_inline("and"), TokenKind::Kw(Kw::And)),
+    (SmolStr::new_inline("apply"), TokenKind::Kw(Kw::Apply)),
+    (SmolStr::new_inline("as"), TokenKind::Kw(Kw::As)),
+    (SmolStr::new_inline("async"), TokenKind::Kw(Kw::Async)),
+    (SmolStr::new_inline("await"), TokenKind::Kw(Kw::Await)),
+    (SmolStr::new_inline("else"), TokenKind::Kw(Kw::Else)),
+    (SmolStr::new_inline("enum"), TokenKind::Kw(Kw::Enum)),
+    (SmolStr::new_inline("ext"), TokenKind::Kw(Kw::Ext)),
+    (SmolStr::new_inline("false"), TokenKind::Kw(Kw::False)),
+    (SmolStr::new_inline("Fn"), TokenKind::Kw(Kw::FnUpper)),
+    (SmolStr::new_inline("fn"), TokenKind::Kw(Kw::FnLower)),
+    (SmolStr::new_inline("fun"), TokenKind::Kw(Kw::Fun)),
+    (SmolStr::new_inline("for"), TokenKind::Kw(Kw::For)),
+    (SmolStr::new_inline("if"), TokenKind::Kw(Kw::If)),
+    (SmolStr::new_inline("imu"), TokenKind::Kw(Kw::Imu)),
+    (SmolStr::new_inline("load"), TokenKind::Kw(Kw::Load)),
+    (SmolStr::new_inline("loop"), TokenKind::Kw(Kw::Loop)),
+    (SmolStr::new_inline("match"), TokenKind::Kw(Kw::Match)),
+    (SmolStr::new_inline("me"), TokenKind::Kw(Kw::Me)),
+    (SmolStr::new_inline("mut"), TokenKind::Kw(Kw::Mut)),
+    (SmolStr::new_inline("pack"), TokenKind::Kw(Kw::Pack)),
+    (SmolStr::new_inline("pub"), TokenKind::Kw(Kw::Pub)),
+    (SmolStr::new_inline("return"), TokenKind::Kw(Kw::Return)),
+    (SmolStr::new_inline("skip"), TokenKind::Kw(Kw::Skip)),
+    (SmolStr::new_inline("stop"), TokenKind::Kw(Kw::Stop)),
+    (SmolStr::new_inline("struct"), TokenKind::Kw(Kw::Struct)),
+    (SmolStr::new_inline("true"), TokenKind::Kw(Kw::True)),
+    (SmolStr::new_inline("type"), TokenKind::Kw(Kw::Type)),
+    (SmolStr::new_inline("_"), TokenKind::Kw(Kw::Underscore)),
+    (SmolStr::new_inline("val"), TokenKind::Kw(Kw::Val)),
+    (SmolStr::new_inline("wasm"), TokenKind::Kw(Kw::Wasm)),
+    (SmolStr::new_inline("when"), TokenKind::Kw(Kw::When)),
+    (SmolStr::new_inline("while"), TokenKind::Kw(Kw::While)),
   ]);
 }
--- crates/compiler/zo-wasm/src/lib.rs
+++ crates/compiler/zo-wasm/src/lib.rs
@@ -1,3 +1,11 @@
+use zo_analyzer::analyzer;
+use zo_interpreter::interpreter;
+use zo_parser::parser;
+use zo_session::backend::Backend;
+use zo_session::session::SESSION;
+use zo_session::settings::Settings;
+use zo_tokenizer::tokenizer;
+
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -7,21 +15,50 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub struct Repl {}
+pub struct Repl {
+  value: JsValue,
+}
 
+#[wasm_bindgen]
 impl Repl {
-  #[inline]
+  #[wasm_bindgen(constructor)]
+  #[inline(always)]
   pub fn new() -> Self {
-    Self {}
+    Self {
+      value: JsValue::from_str(""),
+    }
   }
 
-  #[inline]
-  pub fn add_value(mut self, value: &str) -> Self {
+  #[wasm_bindgen]
+  #[inline(always)]
+  pub fn add_value(&mut self, value: JsValue) {
     self.value = value;
   }
 
+  #[wasm_bindgen]
   #[inline]
-  pub fn eval(self) -> String {
-    String::with_capacity(0usize)
+  pub fn eval(&self) -> Result<JsValue, JsValue> {
+    let session = std::sync::Arc::clone(&SESSION);
+    let mut session = session.lock().unwrap();
+
+    session.with_settings(Settings {
+      backend: Backend::Zo,
+      interactive: std::sync::Arc::new(std::sync::atomic::AtomicBool::new(
+        true,
+      )),
+      ..Default::default()
+    });
+
+    // drop(session);
+
+    let tokens =
+      tokenizer::tokenize(&mut session, &self.value.as_string().unwrap())
+        .unwrap();
+
+    let ast = parser::parse(&mut session, &tokens).unwrap();
+    let ast = analyzer::analyze(&mut session, &ast).unwrap();
+    let value = interpreter::interpret(&mut session, &ast).unwrap();
+
+    Ok(JsValue::from_str(&format!("{value}")))
   }
 }
